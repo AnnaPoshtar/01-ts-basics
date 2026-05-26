f0d37c03cc2689a3fac603cf10d24913f6188800
Anna Poshtar <annaposhtar11@gmail.com>
2026-05-26 18:01:12 +0200

    01-ts-basics

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app!.innerHTML = `
+  <h1>Мої задачі TypeScript</h1>
+`;
+
+import "./task-1";
+import "./task-2";
+import "./task-3";
+import "./task-4";
+import "./task-5";
+import "./task-6";
+import "./task-7";
+import "./task-8";
+console.log("Всі задачі завантажені!");
